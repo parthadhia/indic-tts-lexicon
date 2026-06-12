964abe649bb7e15472c4082838f6877f7528609c
Parth Adhia <parth.adhia@gmail.com>
2026-06-11 21:41:58 -0500

    Add package entrypoint

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+export {
+  loadLexicon,
+  replaceTerms,
+  replaceTermsToSSML
+} from "./replace.js";
+
+export type {
+  LexiconEntry,
+  LexiconLanguage,
+  ReplaceOptions,
+  ReplacementMode,
+  SsmlPhoneme
+} from "./replace.js";
